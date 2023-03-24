490d517b44a719e143ae7b9420a87dcad1d45108
Emerson452 <emerson452pro@gmail.com>
2023-03-24 17:22:40 +0100

    🎨 🚸 add enabledamping

--- src/components/GasStationShow.js
+++ src/components/GasStationShow.js
@@ -23,16 +23,17 @@ import {
     );
   }
   
-  export function GasStationShow(props) {
+  export function GasStationShow() {
 
     return (
       <>
         <Environment 
             files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
             background={"both"}
+            // both = afficher l'environnement HDR comme fond et comme réflexion de la scène
         />
         
-        <OrbitControls target={[10, 1, 7]} maxPolarAngle={1.60} maxDistance={100}/>
+        <OrbitControls target={[10, 1, 7]} maxPolarAngle={1.60} maxDistance={100} enableDamping/>
         {/* target = point d'interet, maxPolarAngle = angle max par rapport au sol*/}
   
         <PerspectiveCamera makeDefault fov={8} position={[75, 25, -25]} />
