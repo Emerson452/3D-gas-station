2958461a6cc181fbbcaeeb78874040a3f98222b0
Emerson452 <emerson452pro@gmail.com>
2023-06-30 14:01:46 +0200

    ✨ Rotation static

--- src/components/GasStation.js
+++ src/components/GasStation.js
@@ -2,47 +2,37 @@ import React, { useEffect } from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-
-// based on "Porsche 911 C4S 2020 Aerokit" (https://sketchfab.com/3d-models/porsche-911-c4s-2020-aerokit-ee69c474a1cc4085944cde4cb7ae87db)
-// by JK3Dstudios (https://sketchfab.com/JK3Dstudio) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
 export function GasStation() {
-    //chargement du modèle 3D
-        const gltf = useLoader(
-          GLTFLoader,
-          process.env.PUBLIC_URL + "models/gasstation/gaz4save.gltf"
-        );
+  const gltf = useLoader(
+    GLTFLoader,
+    process.env.PUBLIC_URL + "models/gasstation/gaz4save.gltf"
+  );
 
-        useEffect(() => {
-          const tvObject = gltf.scene.getObjectByName("Pompe");
-      
-          if (tvObject) {
-            // Faites des modifications sur l'objet ici
-            // Par exemple, changer la couleur du matériau de l'objet
-            tvObject.traverse(function (child) {
-              if (child.isMesh) {
-                child.material.color.set(0xff0000); // Rouge
-              }
-            });
-          }
-        }, [gltf]);
-  //config du modèle 
-  // useEffect(() => {
-  //   gltf.scene.scale.set(1, 1, 1);
-  //   gltf.scene.position.set(0, -0.035, 0);
-  //   gltf.scene.traverse((object) => {
-  //     if (object instanceof Mesh) {
-  //       object.castShadow = true;
-  //       object.receiveShadow = true;
-  //       object.material.envMapIntensity = 20;
-  //     }
-  //   });
-  //   const ambientLight = new AmbientLight(0x404040); // Couleur gris foncé
-  //   gltf.scene.add(ambientLight);
+  useEffect(() => {
+    const pumpObject = gltf.scene.getObjectByName("Pompe");
 
-  //   const directionalLight = new DirectionalLight(0xffffff, 1); // Couleur blanche, intensité de 1
-  //   directionalLight.position.set(0, 10, 10); // Position de la lumière
-  //   gltf.scene.add(directionalLight);
-  // }, [gltf]);
+    if (pumpObject) {
+      const animateRotation = () => {
+        pumpObject.rotation.y += 1.01; // Modifier la vitesse de rotation selon vos besoins
+      };
+
+      const animationId = requestAnimationFrame(animateRotation);
+
+      return () => cancelAnimationFrame(animationId);
+    }
+  }, [gltf]);
+
+  useEffect(() => {
+    const pumpObject = gltf.scene.getObjectByName("Pompe");
+
+    if (pumpObject) {
+      pumpObject.traverse(function (child) {
+        if (child.isMesh) {
+          child.material.color.set(0xff0000); // Modifier la couleur du matériau de l'objet
+        }
+      });
+    }
+  }, [gltf]);
 
   return <primitive object={gltf.scene} />;
 }
