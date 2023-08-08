e177ce92971b728b2a2f3b1a7c8e70d7d9a9fd82
Emerson452 <emerson452pro@gmail.com>
2023-08-08 20:30:48 +0200

    🔥 Remove envmap and some textures

--- src/components/GasStationShow.js
+++ src/components/GasStationShow.js
@@ -1,6 +1,4 @@
 import {
-  CubeCamera,
-  Environment,
   OrbitControls,
   PerspectiveCamera,
 } from "@react-three/drei";
@@ -8,27 +6,12 @@ import { GasStation } from "./GasStation";
 import { Ground } from "./Ground";
 import { Porsche } from "./Porsche";
 
-function GasStationWithEnvironment() {
-  return (
-    <CubeCamera resolution={25} frames={Infinity}>
-      {(texture) => (
-        <>
-          <Environment map={texture} />
-          <GasStation />
-        </>
-      )}
-    </CubeCamera>
-  );
-}
+
 
 export function GasStationShow() {
   return (
     <>
-      <Environment
-        files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
-        background={"both"}
-        // both = afficher l'environnement HDR comme fond et comme réflexion de la scène
-      />
+
 
       <OrbitControls
         target={[10, 1, 7]}
@@ -40,7 +23,7 @@ export function GasStationShow() {
 
       <PerspectiveCamera makeDefault fov={8} position={[75, 25, -25]} />
       <spotLight color={0xffffff} intensity={0.6} position={[30, 10, 10]} />
-      <GasStationWithEnvironment />
+      <GasStation />
       <Porsche />
       <Ground />
     </>
