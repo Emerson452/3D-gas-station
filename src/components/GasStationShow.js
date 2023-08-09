04b7f823f6efe631e3e1708501db52c6b2cf0187
Emerson452 <emerson452pro@gmail.com>
2023-08-09 14:02:55 +0200

    💄 destructure spotlight, for change color

--- src/components/GasStationShow.js
+++ src/components/GasStationShow.js
@@ -1,15 +1,17 @@
-import {
-  OrbitControls,
-  PerspectiveCamera,
-} from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { GasStation } from "./GasStation";
 import { Ground } from "./Ground";
 import { Porsche } from "./Porsche";
 import { useState } from "react";
 
-
-export function CustomButton({ onClick, children, position, rotation }) {
-  
+export function CustomButton({
+  onClick,
+  children,
+  position,
+  rotation,
+  spotlightOn,
+  lightColor
+}) {
   return (
     <group onClick={onClick} position={position} rotation={rotation}>
       <mesh>
@@ -17,6 +19,9 @@ export function CustomButton({ onClick, children, position, rotation }) {
         <meshBasicMaterial color="white" />
       </mesh>
       {children}
+      {spotlightOn && (
+        <spotLight color={lightColor} intensity={0.2} position={[0, 1, 0]} />
+      )}
     </group>
   );
 }
@@ -27,12 +32,24 @@ export function GasStationShow() {
 
   return (
     <>
-{/* Boutons pour contrôler les spotlights */}
-<CustomButton onClick={() => setSpotlight1On(!spotlight1On)} position={[15, 0.01, -2]} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
-        {spotlight1On ? 'Off 1' : 'On 1'}
+      {/* Boutons pour contrôler les spotlights */}
+      <CustomButton
+        onClick={() => setSpotlight1On(!spotlight1On)}
+        position={[15, 0.01, -2]}
+        rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+        spotlightOn={spotlight1On}
+        lightColor={0xffff00}
+      >
+        {spotlight1On ? "Off 1" : "On 1"}
       </CustomButton>
-      <CustomButton onClick={() => setSpotlight2On(!spotlight2On)} position={[14, 0.01, -2]} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
-        {spotlight2On ? 'Off 2' : 'On 2'}
+      <CustomButton
+        onClick={() => setSpotlight2On(!spotlight2On)}
+        position={[14, 0.01, -2]}
+        rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+        spotlightOn={spotlight2On}
+        lightColor={0xff00ff}
+      >
+        {spotlight2On ? "Off 2" : "On 2"}
       </CustomButton>
 
       <OrbitControls
@@ -46,17 +63,17 @@ export function GasStationShow() {
       <PerspectiveCamera makeDefault fov={8} position={[75, 25, -25]} />
 
       <spotLight
-  color={0xffffff}
-  intensity={0.6}
-  position={[30, 10, 10]}
-  visible={spotlight1On}
-/>
-<spotLight
-  color={0xffffff}
-  intensity={0.6}
-  position={[20, 10, 30]}
-  visible={spotlight2On}
-/>
+        color={0xffffff}
+        intensity={0.6}
+        position={[30, 10, 10]}
+        visible={spotlight1On}
+      />
+      <spotLight
+        color={0xffffff}
+        intensity={0.6}
+        position={[20, 10, 30]}
+        visible={spotlight2On}
+      />
 
       <spotLight color={0xffffff} intensity={0.6} position={[30, 10, 10]} />
       <GasStation />
