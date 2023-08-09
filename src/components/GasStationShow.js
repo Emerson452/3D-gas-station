ddad5e2b6da3eb69d22a0e4c11d990cc40643000
Emerson452 <emerson452pro@gmail.com>
2023-08-09 12:48:48 +0200

    🎨 add somes lights and button controlers

--- src/components/GasStationShow.js
+++ src/components/GasStationShow.js
@@ -5,13 +5,32 @@ import {
 import { GasStation } from "./GasStation";
 import { Ground } from "./Ground";
 import { Porsche } from "./Porsche";
+import { useState } from "react";
 
 
+export function CustomButton({ onClick, children }) {
+  return (
+    <mesh onClick={onClick}>
+      <planeGeometry args={[1, 0.2]} />
+      <meshBasicMaterial color="white" />
+        {children}
+    </mesh>
+  );
+}
 
 export function GasStationShow() {
+  const [spotlight1On, setSpotlight1On] = useState(false);
+  const [spotlight2On, setSpotlight2On] = useState(false);
+
   return (
     <>
-
+{/* Boutons pour contrôler les spotlights */}
+<CustomButton onClick={() => setSpotlight1On(!spotlight1On)}>
+        {spotlight1On ? 'Éteindre Spotlight 1' : 'Allumer Spotlight 1'}
+      </CustomButton>
+      <CustomButton onClick={() => setSpotlight2On(!spotlight2On)}>
+        {spotlight2On ? 'Éteindre Spotlight 2' : 'Allumer Spotlight 2'}
+      </CustomButton>
 
       <OrbitControls
         target={[10, 1, 7]}
@@ -22,6 +41,20 @@ export function GasStationShow() {
       {/* target = point d'interet, maxPolarAngle = angle max par rapport au sol*/}
 
       <PerspectiveCamera makeDefault fov={8} position={[75, 25, -25]} />
+
+      <spotLight
+  color={0xffffff}
+  intensity={0.6}
+  position={[30, 10, 10]}
+  visible={spotlight1On}
+/>
+<spotLight
+  color={0xffffff}
+  intensity={0.6}
+  position={[20, 10, 30]}
+  visible={spotlight2On}
+/>
+
       <spotLight color={0xffffff} intensity={0.6} position={[30, 10, 10]} />
       <GasStation />
       <Porsche />
